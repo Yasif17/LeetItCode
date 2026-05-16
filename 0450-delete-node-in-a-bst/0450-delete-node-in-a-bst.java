f03971bf823e8853e6abc92c82f92d809fce942d
Yasif khan <125533092+Yasif17@users.noreply.github.com>
2026-05-17 02:11:03 +0530

    Time: 0 ms (100%), Space: 47.5 MB (54.42%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -16,31 +16,44 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root==null) return null;
-       if(root.val>key){
-        root.left = deleteNode(root.left,key);
-       }else if(root.val<key){
-        root.right=deleteNode(root.right,key);
-       }else{
-        if(root.left==null){
-          return root.right;
-        }
-        if(root.right==null){
-          return root.left;
-        }
-        TreeNode inorderSuccesor = findSmallest(root.right);
-        root.val = inorderSuccesor.val;
 
-        root.right = deleteNode(root.right,inorderSuccesor.val);
+        if(key<root.val){
+            root.left = deleteNode(root.left,key);
+        }
+        else if(key>root.val){
+            root.right = deleteNode(root.right,key);
+        }
+        else{
 
-       }
-         return root;
+        if(root.left==null && root.right==null) return null;
+
+        if(root.left==null) return root.right;
+
+        if(root.right==null) return root.left;
+
+        TreeNode successor = findMin(root.right);
+
+        root.val = successor.val;
+
+        root.right = deleteNode(root.right,successor.val);
+        }
+
+        return root;
+
     }
 
-      static TreeNode findSmallest(TreeNode root) {
+    public TreeNode findMin(TreeNode root){
         TreeNode temp = root;
-        while (temp.left != null) {
+        while(temp.left!=null){
             temp = temp.left;
         }
         return temp;
     }
+
 }
+
+
+
+
+
+
